370d54a24e6d785c00d6545a1d134651d1f1f3fd
JohnstoneDev <marucha.webdev@yahoo.com>
2023-01-31 22:00:43 +0300

    Final setup & configuration

--- src/App.js
+++ src/App.js
@@ -2,17 +2,16 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header bg-black bg-blend-darken">
-        <h1 className='text-4xl text-slate-300 animate-pulse'> React Template Repository with the following packages Pre-installed :</h1>      
-        <ol className='pt-5'>
-           <li>React Router (v5) </li>
-           <li>Tailwind CSS </li>
-            <li> Configuration to use a proxy server.</li>
-            <li>React start script modified to use openssl legacy provider.</li>
-        </ol>
-      
-      </header>
+    <div className="App font-main  bg-main-bg text-heading leading-5">
+      <h2 className=" text-heading p-2 text-4xl font-semibold animate-pulse border-b-4 border-modal-bg">This is the Admin side </h2>
+      <div className=" flex  flex-col p-2 text-left items-center align-middle justify-center">
+          elements that should be here : 
+          <ol className='p-3'>
+              <li>A form to add the elements to the db(add inventory items) </li>
+              <li>A Section to complete orders </li>
+              <li>Confirmation of payments / total items sold </li>
+          </ol>
+      </div>
     </div>
   );
 }
